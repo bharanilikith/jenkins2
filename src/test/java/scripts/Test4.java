d271044ec1f4d8c54fa6ff9da887497a2f51b603
QSP <QSP@QSP>
2022-07-15 08:29:37 +0530

    Hi

---
+++ src/test/java/scripts/Test4.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 {
+	@Test
+	public void tc6() {
+		Reporter.log("hello Appium",true);
+	}
+
+}
---
+++ src/test/java/scripts/Test5.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5 {
+	@Test
+	public void tc5() {
+		Reporter.log("hello API",true);
+	}
+
+}
